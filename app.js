56edd5a9021ac90272ff0a08f4c843093c6c8a67
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-05-16 22:53:42 +0900

    five commit

--- app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at <https://localhost>:${PORT}`);
-});
